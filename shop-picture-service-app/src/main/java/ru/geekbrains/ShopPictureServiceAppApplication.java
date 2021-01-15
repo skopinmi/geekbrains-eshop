60a52dd2fc1ece68c598c00bac676da17880a24c
Skopin <Skopin_MI@mail.ru>
2021-01-15 13:05:30 +0300

    getaway-service
    берет настройки с config-server
    подключается к eureka

--- shop-picture-service-app/src/main/java/ru/geekbrains/ShopPictureServiceAppApplication.java
+++ shop-picture-service-app/src/main/java/ru/geekbrains/ShopPictureServiceAppApplication.java
@@ -3,6 +3,7 @@ package ru.geekbrains;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ShopPictureServiceAppApplication {
 
